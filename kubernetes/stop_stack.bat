207daf21eed547627dc647ee55e40b907589ee95
Jignesh Makwana <makwana.jignesh@gmail.com>
2020-06-05 10:42:54 +0800

    added file to stop stack

--- kubernetes/stop_stack.bat
+++ kubernetes/stop_stack.bat
@@ -4,13 +4,13 @@ cd C:\mylab\lab
 
 Rem start node1
 cd kubernetes\k8node1
-vagrant up
+vagrant down
 
 Rem start node2
 cd kubernetes\k8node2
-vagrant up
+vagrant down
 
 Rem start node3
 cd kubernetes\k8node3
-vagrant up
+vagrant down
 
