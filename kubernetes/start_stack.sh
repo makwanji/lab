9d3dac6b6293e6e74d83bc74714de4744a7aa288
makwanji <makwana.jignesh@gmail.com>
2020-08-07 19:03:13 +0800

    change workder node file

--- kubernetes/start_stack.sh
+++ kubernetes/start_stack.sh
@@ -5,6 +5,15 @@ echo "set location of your home folder"
 export LAB_HOME_LOC=/home/jig/lab/
 echo "$LAB_HOME_LOC"
 
+
+echo "start master node1"
+cd $LAB_HOME_LOC/kubernetes/k8m1
+vagrant up
+
+echo "start master node2"
+cd $LAB_HOME_LOC/kubernetes/k8m2
+vagrant up
+
 echo "start node1"
 cd $LAB_HOME_LOC/kubernetes/k8node1
 vagrant up
@@ -13,6 +22,10 @@ echo "start node2"
 cd $LAB_HOME_LOC/kubernetes/k8node2
 vagrant up
 
-echo "start node2"
+echo "start node3"
 cd $LAB_HOME_LOC/kubernetes/k8node3
+vagrant up
+
+echo "start node4"
+cd $LAB_HOME_LOC/kubernetes/k8node4
 vagrant up
--- kubernetes/stop_stack.sh
+++ kubernetes/stop_stack.sh
@@ -4,6 +4,14 @@ echo "set location of your home folder"
 export LAB_HOME_LOC=/home/jig/lab/
 echo "$LAB_HOME_LOC"
 
+echo "stop master node1"
+cd $LAB_HOME_LOC/kubernetes/k8m1
+vagrant halt
+
+echo "stop master node2"
+cd $LAB_HOME_LOC/kubernetes/k8m2
+vagrant halt
+
 echo "stop node1"
 cd $LAB_HOME_LOC/kubernetes/k8node1
 vagrant halt
@@ -14,4 +22,8 @@ vagrant halt
 
 echo "stop node3"
 cd $LAB_HOME_LOC/kubernetes/k8node3
+vagrant halt
+
+echo "stop node4"
+cd $LAB_HOME_LOC/kubernetes/k8node4
 vagrant halt
